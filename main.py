c8c7ebef144ec17c4082375a023af7c4083e5e2d
Gitpho <gitpho@gmail.com>
2026-06-02 16:26:44 -0400

    4.5 Asteroids | Destruction

--- main.py
+++ main.py
@@ -46,6 +46,12 @@ def main() -> None:
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill("black")
 
         for obj in drawable:
